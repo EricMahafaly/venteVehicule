3283e2e265fda7c0dd42a3ad2540cfa709553733
EricMahafaly <Ericmahafaly7@gmail.com>
2024-02-09 02:36:41 +0100

    uploadFile

--- src/main/java/com/Eric/venteVehicule/service/FileService.java
+++ src/main/java/com/Eric/venteVehicule/service/FileService.java
@@ -1,5 +1,7 @@
 package com.Eric.venteVehicule.service;
 
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,7 +16,7 @@ public class FileService {
     private  String imageMountPath;
     public String saveFile(MultipartFile file) throws Exception {
         Path targetPath = Path.of(imageMountPath, file.getOriginalFilename());
-        Files.copy(file.getInputStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+        FileUtils.writeByteArrayToFile(targetPath.toFile(), file.getBytes());
 
         return targetPath.toString();
     }
